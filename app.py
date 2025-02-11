15c207dacd60030155befe601901664143c35cb5
MarjanCode <147977132+MarjanCode@users.noreply.github.com>
2025-02-11 07:31:16 +0000

    Tue, Feb 11, 2025, 11:01 AM +03:30

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    lang = request.args.get("lang", "per")  # Default to Persian
+    lang = request.args.get("lang", "fa")  # Default to Farsi
     return render_template("index.html", lang=lang)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
